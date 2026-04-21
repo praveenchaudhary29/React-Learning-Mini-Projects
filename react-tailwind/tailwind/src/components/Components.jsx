9755654ab57759c20f9d3856b603a26ff74b3fa2
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-21 23:20:37 +0530

    Update Components.jsx

--- react-tailwind/tailwind/src/components/Components.jsx
+++ react-tailwind/tailwind/src/components/Components.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Component(props){
   return <>
-  <div className=" w-72 flex flex-col rounded-xl glass  min-h-72 mt-3 ">
+  <div className=" w-72 flex  flex-col rounded-xl glass  min-h-72 mt-3 ">
         <div>
           <img
             src="https://media4.giphy.com/media/1gbqIc1fK8QgR3bHL7/giphy.gif?cid=790b7611a2f696d51a46ce892e420e77735707466a4abd3b&rid=giphy.gif&ct=g"
